a8d8e661c88256dcd28b986b02887bf1b9eb45db
Wilco Boshoff <wlboshoff@gmail.com>
2023-11-28 07:26:23 +0200

    Animations
    We've added animations for the shadow of a fan-light and for the
    explosion of a grenade.

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -3,3 +3,10 @@ extends RigidBody2D
 class_name Grenade
 
 const speed: int = 1000
+
+@onready var explosion: Sprite2D = $Explosion
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func explode() -> void:
+	explosion.visible = true
+	animation_player.play("explosion")
