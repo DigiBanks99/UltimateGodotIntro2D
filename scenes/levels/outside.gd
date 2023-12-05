6a4d0a38abe7d639f24a6ff6140747bb4c51b52a
Wilco Boshoff <wlboshoff@gmail.com>
2023-12-05 07:46:36 +0200

    Level transition
    We've added level transitions to the inside/outside levels. Note that I
    tried to copy the PackedScene with a preload, but then the signals seem
    to get disconnected when loading a scene a second time, so I switched
    away from that method

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -3,6 +3,7 @@ extends Level
 func _on_gate_player_entered_gate(_body: Node2D) -> void:
 	var tween: Tween = create_tween()
 	tween.tween_property(player, 'speed', 0, 0.5)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
 
 
 func _on_house_player_entered() -> void:
